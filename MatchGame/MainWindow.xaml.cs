9cfaeff8d2caaa20d8baaacb804faa03670f0c3a
kent1201 <kent19961201@gmail.com>
2021-10-18 11:59:53 +0800

    UI completed

--- MatchGame/MainWindow.xaml.cs
+++ MatchGame/MainWindow.xaml.cs
@@ -23,6 +23,32 @@ namespace MatchGame
         public MainWindow()
         {
             InitializeComponent();
+
+            SetUpGame();
+        }
+
+        private void SetUpGame()
+        {
+            List<string> animalEmoji = new List<string>()
+            {
+                "😺", "😺",
+                "🦊", "🦊",
+                "🐭", "🐭",
+                "🐹", "🐹",
+                "🐰", "🐰",
+                "🐶", "🐶",
+                "🐺", "🐺",
+                "🐨", "🐨"
+            };
+            Random random = new Random();
+            foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
+            {
+                int index = random.Next(animalEmoji.Count);
+                string nextEmoji = animalEmoji[index];
+                textBlock.Text = nextEmoji;
+                animalEmoji.RemoveAt(index);
+            }
+            
         }
     }
 }
